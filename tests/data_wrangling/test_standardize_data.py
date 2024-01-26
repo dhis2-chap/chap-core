e8027cb9f436380c73264e1264d9c43b5bf236e6
Sveinung Gundersen <sveinugu@gmail.com>
2024-01-26 12:26:46 +0100

    Loading of separated data using omnipy

---
+++ tests/data_wrangling/test_standardize_data.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from omnipy import StrDataset
+
+from climate_health.data_wrangling.separated_data import load_separated_data
+
+
+def test_load_separated_data():
+    example_data_path = Path(__file__).parent.parent.parent / 'example_data'
+    data_files = (str(example_data_path / filename) for filename in
+                  ('separated_disease_data.csv',
+                   'separated_rain_data.csv',
+                   'separated_temp_data.csv'))
+    dataset = load_separated_data(data_files)
+    assert isinstance(dataset, StrDataset)
+    assert len(dataset) == 3
+    assert tuple(dataset.keys()) == ('separated_disease_data',
+                                     'separated_rain_data',
+                                     'separated_temp_data')
+
+
+def test_standardize_separated_data():
+    ...
