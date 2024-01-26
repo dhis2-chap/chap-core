a4deafa41019223005d9f837e31811f74136a604
Jeanne Cheneby <jeannech@uio.no>
2024-01-26 11:24:32 +0100

    Add from_string function

--- tests/time_period/test_data_objects.py
+++ tests/time_period/test_data_objects.py
@@ -3,7 +3,6 @@ import pytest
 from climate_health.time_period import TimePeriod, Month
 
 
-@pytest.mark.xfail
 def test_time_period():
-    month = TimePeriod.from_str('2013-01')
+    month = TimePeriod.from_string('2013-01')
     assert str(month) == 'January 2013'
