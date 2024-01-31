b20049fb537b7d0d55a1ca543a565abfabf76483
kanduric <chakri.co@gmail.com>
2024-01-31 14:14:31 +0100

    fixes a bug in from_csv and to_csv, where wrong column names were used

--- climate_health/datatypes.py
+++ climate_health/datatypes.py
@@ -12,16 +12,16 @@ class ClimateHealthTimeSeries:
     @classmethod
     def from_csv(cls, csv_file: str, **kwargs):
         """Read data from a csv file."""
-        data = pd.read_csv(csv_file, dtype={'time_period': str}, **kwargs)
-        return cls(data.time_period, data.rainfall, data.mean_temperature, data.disease_cases)
+        data = pd.read_csv(csv_file, dtype={'Time': str}, **kwargs)
+        return cls(data.Time, data.Rain, data.Temperature, data.Disease)
 
     def to_csv(self, csv_file: str, **kwargs):
         """Write data to a csv file."""
         data = pd.DataFrame({
-            "time_period": self.time_period,
-            "rainfall": self.rainfall,
-            "mean_temperature": self.mean_temperature,
-            "disease_cases": self.disease_cases,
+            "Time": self.time_period,
+            "Rain": self.rainfall,
+            "Temperature": self.mean_temperature,
+            "Disease": self.disease_cases,
         })
         data.to_csv(csv_file, index=False, **kwargs)
 
--- tests/test_datatypes.py
+++ tests/test_datatypes.py
@@ -9,10 +9,10 @@ def test_climate_health_time_series_from_csv(tmp_path):
     """Test the from_csv method."""
     data = pd.DataFrame(
         {
-            "time_period": ["2010", "2011", "2012"],
-            "rainfall": [1.0, 2.0, 3.0],
-            "mean_temperature": [1.0, 2.0, 3.0],
-            "disease_cases": [1, 2, 3],
+            "Time": ["2010", "2011", "2012"],
+            "Rain": [1.0, 2.0, 3.0],
+            "Temperature": [1.0, 2.0, 3.0],
+            "Disease": [1, 2, 3],
         }
     )
     csv_file = tmp_path / "test.csv"
@@ -28,15 +28,12 @@ def test_climate_health_time_series_from_csv(tmp_path):
 def test_climate_health_time_series_to_csv(tmp_path):
     """Test the to_csv method."""
     ts = ClimateHealthTimeSeries(
-        time_period=["2010", "2011", "2012"],
+        time_period=bnp.as_encoded_array(["2010", "2011", "2012"]),
         rainfall=np.array([1.0, 2.0, 3.0]),
         mean_temperature=np.array([1.0, 2.0, 3.0]),
         disease_cases=np.array([1, 2, 3]),
     )
     csv_file = tmp_path / "test.csv"
     ts.to_csv(csv_file)
-    data = pd.read_csv(csv_file, dtype={'time_period': str})
-    assert data.time_period.tolist() == ["2010", "2011", "2012"]
-    assert data.rainfall.tolist() == [1.0, 2.0, 3.0]
-    assert data.mean_temperature.tolist() == [1.0, 2.0, 3.0]
-    assert data.disease_cases.tolist() == [1, 2, 3]
+    ts2 = ClimateHealthTimeSeries.from_csv(csv_file)
+    assert ts == ts2
