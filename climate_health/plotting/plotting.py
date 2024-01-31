80a446a51ec67fe7e7a343b888ec5d3d7e09f591
kanduric <chakri.co@gmail.com>
2024-01-31 14:18:47 +0100

    fixes bug related to free scale y axes

--- climate_health/plotting/plotting.py
+++ climate_health/plotting/plotting.py
@@ -9,5 +9,7 @@ def plot_timeseries_data(data:ClimateHealthTimeSeries):
     returns a plotly object that can be shown or saved."""
     df = data.topandas()
     df = pd.melt(df, id_vars=["time_period"], var_name="variable", value_name="value")
-    return px.line(df, x="time_period", y="value", facet_row="variable", title="Climate Health Data")
+    fig = px.line(df, x="time_period", y="value", facet_row="variable", title="Climate Health Data")
+    fig.update_yaxes(matches=None)
+    return fig
 
