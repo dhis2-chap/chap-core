4d1a7490189549697efc77e8d06d967ab9f83525
Ivar Grytten <ivar.grytten@gmail.com>
2024-02-06 09:49:56 +0100

    changed failing season simulator test

--- tests/simulation/test_seasonal_simulator.py
+++ tests/simulation/test_seasonal_simulator.py
@@ -4,8 +4,7 @@ from climate_health.simulation.seasonal_simulator import SeasonalSingleVariableS
 def test_seasonal_single_variable_simulator():
     """Test the SeasonalSingleVariableSimulator class."""
     simulator = SeasonalSingleVariableSimulator(
-        n_seasons=5, n_data_points_per_season=10, max_peak_height=100
+        n_seasons=5, n_data_points_per_season=10, mean_peak_height=100, peak_height_sd=10
     )
     data = simulator.simulate()
     assert len(data) == 50
-    assert data.max() <= 100
