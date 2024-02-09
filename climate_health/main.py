bf85d5b33961e6a71d06738394664905589c7a34
Ivar Grytten <ivar.grytten@gmail.com>
2024-02-09 14:21:24 +0100

    Some cleanup

--- climate_health/main.py
+++ climate_health/main.py
@@ -7,6 +7,30 @@ import pandas as pd
 from collections import defaultdict
 from climate_health.datatypes import ClimateHealthTimeSeries
 
+
+def assess_model_on_csv_data(data_file_name: str, split_fraction: float,
+                             model: 'PlaceholderModel') -> 'AssessmentReport':
+    """
+    Wraps together all necessary steps for reading data from file, assessing a model on different lags ahead
+    and generating a report with results.
+    """
+    data = ClimateHealthTimeSeries.from_csv(data_file_name)
+    data = data.topandas()
+
+    prediction_dict = defaultdict(lambda: defaultdict(int))
+    truth_dict = defaultdict(lambda: defaultdict(int))
+    for lag_ahead in range(1, 10):
+        rowbased_data = lagged_rows(data, lag_rows=[3], lag=lag_ahead)
+        x_train, y_train, x_test, y_test = (
+            split_to_train_test_truth_fixed_ahead_lag(rowbased_data, split_fraction))
+        model.fit(x_train, y_train)
+        for test_time_offset, (single_X_test, single_Y_test) in enumerate(zip(x_test, y_test)):
+            prediction_dict[lag_ahead][test_time_offset] = model.predict(single_X_test)
+            truth_dict[lag_ahead][test_time_offset] = single_Y_test
+    report = make_assessment_report(prediction_dict, truth_dict)
+    return report
+
+
 def lagged_rows(data, lag_rows, lag=1):
     new_data = data.iloc[lag:]
     for lag_row in lag_rows:
@@ -16,7 +40,6 @@ def lagged_rows(data, lag_rows, lag=1):
     return new_data
 
 
-# %%
 def split_to_train_test_truth_fixed_ahead_lag(data, split_fraction):
     hardcoded_test_col = 3
     n = data.shape[0]
@@ -37,10 +60,13 @@ class PlaceholderModel:
     def __init__(self):
         return
 
-    def fit(self, X_train, Y_train):
+    def fit(self, x_train, y_train):
         return self
 
-    def predict(self, single_X_test):
+    def predict(self, single_x_test):
+        """
+        Takes a single test data point and returns prediction
+        """
         return 0
 
 
@@ -52,22 +78,4 @@ class AssessmentReport:
 
 def make_assessment_report(prediction_dict, truth_dict) -> AssessmentReport:
     return AssessmentReport("Good job!")
-
-def assess_model_on_csv_data(data_file_name: str, split_fraction: float,
-                           model: PlaceholderModel) -> AssessmentReport:
-    data = ClimateHealthTimeSeries.from_csv(data_file_name)  # real data or simulated data
-    data = data.topandas()
-
-    prediction_dict = defaultdict(lambda: defaultdict(int))
-    truth_dict = defaultdict(lambda: defaultdict(int))
-    for lag_ahead in range(1, 10):
-        rowbased_data = lagged_rows(data, lag_rows=[3], lag=lag_ahead)
-        X_train, Y_train, X_test, Y_test = (
-            split_to_train_test_truth_fixed_ahead_lag(rowbased_data, split_fraction))
-        model.fit(X_train, Y_train)
-        for test_time_offset, (single_X_test, single_Y_test) in enumerate(zip(X_test, Y_test)):
-            prediction_dict[lag_ahead][test_time_offset] = model.predict(single_X_test)
-            truth_dict[lag_ahead][test_time_offset] = single_Y_test
-    report = make_assessment_report(prediction_dict, truth_dict)
-    return report
 
