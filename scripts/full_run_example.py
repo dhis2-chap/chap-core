6aae4567c6a3a70bf1accb8284386b0fc03e6373
Ivar Grytten <ivar.grytten@gmail.com>
2024-02-09 13:34:22 +0100

    Moved integration code, integration test added

--- scripts/full_run_example.py
+++ scripts/full_run_example.py
@@ -1,75 +1,9 @@
 """
 Script for full run of training a model and assessing it on a dataset.
 """
-import pandas as pd
-from collections import defaultdict
-from climate_health.datatypes import ClimateHealthTimeSeries
-
-def lagged_rows(data, lag_rows, lag=1):
-    new_data = data.iloc[lag:]
-    for lag_row in lag_rows:
-        for i in range(lag):
-            lagged_col = data.iloc[lag - i - 1:-1 - i, lag_row].values
-            new_data.insert(len(new_data.columns), f"{data.columns[lag_row]}_lag_{i + 1}", lagged_col)
-    return new_data
-
-
-# %%
-def split_to_train_test_truth_fixed_ahead_lag(data, split_fraction):
-    hardcoded_test_col = 3
-    n = data.shape[0]
-    train_idx = int((n * split_fraction) // 1)
-    X = data.drop(data.columns[hardcoded_test_col], axis=1)
-    Y = data.iloc[:, hardcoded_test_col]
-
-    X_train = X.iloc[:train_idx]
-    X_test = X.iloc[train_idx:]
-    Y_train = Y.iloc[:train_idx]
-    Y_test = Y.iloc[train_idx:]
-
-    return X_train, Y_train, X_test, Y_test
-
-
-class PlaceholderModel:
-
-    def __init__(self):
-        return
-
-    def fit(self, X_train, Y_train):
-        return self
-
-    def predict(self, single_X_test):
-        return 0
-
-
-class AssessmentReport:
-    def __init__(self, text):
-        self.text = text
-        return
-
-
-def make_assessment_report(prediction_dict, truth_dict) -> AssessmentReport:
-    return AssessmentReport("Good job!")
-
-
-def main(fn: str, split_fraction: float,
-                           model: PlaceholderModel) -> AssessmentReport:
-    data = ClimateHealthTimeSeries.from_csv(fn)  # real data or simulated data
-    data = data.topandas()
-
-    prediction_dict = defaultdict(lambda: defaultdict(int))
-    truth_dict = defaultdict(lambda: defaultdict(int))
-    for lag_ahead in range(1, 10):
-        rowbased_data = lagged_rows(data, lag_rows=[3], lag=lag_ahead)
-        X_train, Y_train, X_test, Y_test = split_to_train_test_truth_fixed_ahead_lag(rowbased_data, split_fraction)
-        model.fit(X_train, Y_train)
-        for test_time_offset, (single_X_test, single_Y_test) in enumerate(zip(X_test, Y_test)):
-            prediction_dict[lag_ahead][test_time_offset] = model.predict(single_X_test)
-            truth_dict[lag_ahead][test_time_offset] = single_Y_test
-    report = make_assessment_report(prediction_dict, truth_dict)
-    return report
+from climate_health.main import assess_model_on_csv_data, PlaceholderModel
 
 
 if __name__ == "__main__":
-    report = main("../example_data/data.csv", 0.5, PlaceholderModel())
+    report = assess_model_on_csv_data("../example_data/data.csv", 0.5, PlaceholderModel())
     print(report.text)
---
+++ tests/test_integration.py
@@ -0,0 +1,8 @@
+from climate_health.main import assess_model_on_csv_data, PlaceholderModel
+
+
+def test_full_run():
+    data_file = "example_data/data.csv"
+    report = assess_model_on_csv_data(data_file, 0.5, PlaceholderModel())
+    print(report.text)
+
