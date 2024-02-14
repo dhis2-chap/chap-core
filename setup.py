04a8f5ccbabd88ffbf82bf772f7269bf36324dd2
kanduric <chakri.co@gmail.com>
2024-02-14 13:40:02 +0100

    adds sklearn to setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['typer', 'numpy', 'bionumpy', 'pandas', 'omnipy>=0.14.12', 'plotly']
+requirements = ['typer', 'numpy', 'bionumpy', 'pandas', 'omnipy>=0.14.12', 'plotly', 'sklearn']
 
 test_requirements = ['pytest>=3', "hypothesis"]
 
