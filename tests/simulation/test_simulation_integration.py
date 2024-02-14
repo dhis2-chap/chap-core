eb59ea0d12918d58c4279cf475c7c43f81a95172
kanduric <chakri.co@gmail.com>
2024-02-14 13:43:48 +0100

    add integration test to simulation with main

--- tests/simulation/test_simulation_integration.py
+++ tests/simulation/test_simulation_integration.py
@@ -1,4 +1,5 @@
 from climate_health.main import assess_model_on_csv_data
+from climate_health.predictor.poisson import Poisson
 from climate_health.simulation.random_noise_simulator import RandomNoiseSimulator
 
 
@@ -6,4 +7,5 @@ def test_simulation_integration(tmp_path):
     data = RandomNoiseSimulator(100).simulate()
     file_name = tmp_path / "data.csv"
     data.to_csv(file_name)
-    assess_model_on_csv_data(data_file_name=file_name, split_fraction=0.7,model)
+    report = assess_model_on_csv_data(data_file_name=file_name, split_fraction=0.7, model=Poisson())
+    print(report.rmse_dict)
