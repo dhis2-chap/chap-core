652861a98d5f6374707f6950a9c5fdfd9d96751d
kanduric <chakri.co@gmail.com>
2024-02-14 13:38:32 +0100

    on progress to integration test of simulations with main

---
+++ tests/simulation/test_simulation_integration.py
@@ -0,0 +1,9 @@
+from climate_health.main import assess_model_on_csv_data
+from climate_health.simulation.random_noise_simulator import RandomNoiseSimulator
+
+
+def test_simulation_integration(tmp_path):
+    data = RandomNoiseSimulator(100).simulate()
+    file_name = tmp_path / "data.csv"
+    data.to_csv(file_name)
+    assess_model_on_csv_data(data_file_name=file_name, split_fraction=0.7,model)
