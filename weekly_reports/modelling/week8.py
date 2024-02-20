08da8285969a7345c45961c30959139d0f915cb1
Knut Rand <knutdrand@gmail.com>
2024-02-20 10:46:54 +0100

    working on integration

--- weekly_reports/modelling/week8.py
+++ weekly_reports/modelling/week8.py
@@ -12,7 +12,7 @@ Goals for this week:
 import numpy as np
 import jax
 from probabilistic_machine_learning.cases.diff_encoded_mosquito import diff_encoded_model, simple_model, \
-    model_evaluation_plot, debug_logprob
+    model_evaluation_plot, debug_logprob, advancing_model
 from probabilistic_machine_learning.adaptors.jax_nuts import sample as nuts_sample
 import matplotlib.pyplot as plt
 from report_tests import get_markdown
@@ -68,7 +68,6 @@ def test_run_with_climate_health_data():
 
 
 def get_simulator(sample, real_params):
-
     class Simulator:
         def simulate(self, climate_data: ClimateData):
             T = len(climate_data) + 1
@@ -80,25 +79,29 @@ def get_simulator(sample, real_params):
 
 
 class SimpleSampler:
-    def __init__(self, key,  log_prob: callable, sample_func: callable,  param_names: list[str]):
+    def __init__(self, key, log_prob: callable, sample_func: callable, param_names: list[str], n_states=None):
         self._log_prob = log_prob
         self._param_names = param_names
         self._param_samples = None
-        self._sample_key= key
+        self._sample_key = key
         self._sample_func = sample_func
+        self._n_states = n_states
 
     @property
     def n_samples(self):
         return len(self._param_samples[list(self._param_samples)[0]])
 
-
-    def train(self, time_series: ClimateHealthTimeSeries)-> HealthData:
+    def train(self, time_series: ClimateHealthTimeSeries) -> HealthData:
         T = len(time_series) + 1
-        init_diffs = np.random.normal(0, 1, (T - 1))
-        init_dict = {'logits_array': init_diffs} | {param_name: np.random.normal(0, 10) for param_name in self._param_names}
+        if self._n_states is None:
+            init_diffs = np.random.normal(0, 1, (T - 1))
+        else:
+            init_diffs = np.random.normal(0, 1, (T - 1, self._n_states)).reshape((T - 1, self._n_states))
+        init_dict = {'logits_array': init_diffs} | {param_name: np.random.normal(0, 10) for param_name in
+                                                    self._param_names}
         lp = self._log_prob(time_series.disease_cases, time_series.mean_temperature)
-        self._sample_key, key  = jax.random.split(self._sample_key)
-        self._param_samples = nuts_sample(lp, key, init_dict, 50, 50)
+        self._sample_key, key = jax.random.split(self._sample_key)
+        self._param_samples = nuts_sample(lp, key, init_dict, 50, 500)
 
     def sample(self, climate_data: ClimateData) -> HealthData:
         T = len(climate_data) + 1
@@ -114,17 +117,30 @@ def test_simplified_interface():
     Automate boilerplate code
     '''
 
-    sample, log_prob, reconstruct_state = simple_model()
-    param_names = ['beta']
+    model = lambda: (simple_model(), (['beta'], None))
+    figure = check_model_capacity(model)
+    return figure.show()
+
+
+def test_mored_advanced_model():
+    '''
+    Check model_capacity for full SEIR model
+    '''
+    model = advancing_model
+    return check_model_capacity(model).show()
+
+
+def check_model_capacity(model):
+    (sample, log_prob, reconstruct_state), (param_names, n_states) = model()
     real_params = {name: np.random.normal(0, 10) for name in param_names}
     climate_data = ClimateData.from_csv('../../example_data/climate_data.csv')
     simulator = get_simulator(sample, real_params)
     health_data = simulator.simulate(climate_data)
-    sampler = SimpleSampler(jax.random.PRNGKey(0), log_prob, sample, param_names)
+    sampler = SimpleSampler(jax.random.PRNGKey(0), log_prob, sample, param_names, n_states)
     data_set = ClimateHealthTimeSeries.combine(health_data, climate_data)
     sampler.train(data_set)
     figure = prediction_plot(health_data, sampler, climate_data, 10)
-    return figure.show()
+    return figure
 
 
 if __name__ == '__main__':
