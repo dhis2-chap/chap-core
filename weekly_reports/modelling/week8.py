38c3725397e078c025e578f323518f4b5d922660
Knut Rand <knutdrand@gmail.com>
2024-02-21 15:55:37 +0100

    simulating from a mosquito model

--- weekly_reports/modelling/week8.py
+++ weekly_reports/modelling/week8.py
@@ -13,10 +13,12 @@ import numpy as np
 import jax
 import jax.numpy as jnp
 from probabilistic_machine_learning.cases.diff_encoded_mosquito import diff_encoded_model, simple_model, \
-    model_evaluation_plot, seir_model, mosquito_model, pure_mosquito_model
+    model_evaluation_plot, seir_model, mosquito_model, pure_mosquito_model, get_death_rate, get_maturation_rate_by_temp
 from probabilistic_machine_learning.adaptors.jax_nuts import sample as nuts_sample
 import matplotlib.pyplot as plt
 from report_tests import get_markdown
+from scipy.special import logit
+
 from climate_health.datatypes import ClimateData, HealthData, ClimateHealthTimeSeries
 from climate_health.plotting.prediction_plot import prediction_plot
 
@@ -77,7 +79,8 @@ def get_simulator(sample, real_params):
 
 
 class SimpleSampler:
-    def __init__(self, key, log_prob: callable, sample_func: callable, param_names: list[str], n_states=None, n_warmup_samples=1000):
+    def __init__(self, key, log_prob: callable, sample_func: callable, param_names: list[str], n_states=None,
+                 n_warmup_samples=1000):
         self._log_prob = log_prob
         self._param_names = param_names
         self._param_samples = None
@@ -90,21 +93,35 @@ class SimpleSampler:
     def n_samples(self):
         return len(self._param_samples[list(self._param_samples)[0]])
 
-    def train(self, time_series: ClimateHealthTimeSeries) -> HealthData:
+    def train(self, time_series: ClimateHealthTimeSeries, init_values=None) -> HealthData:
         T = len(time_series) + 1
         if self._n_states is None:
             init_diffs = np.random.normal(0, 1, (T - 1))
         else:
             init_diffs = np.random.normal(0, 1, (T - 1, self._n_states)).reshape((T - 1, self._n_states))
-        init_dict = {'logits_array': init_diffs} | {param_name: np.random.normal(0, 10) for param_name in
-                                                    self._param_names}
+        init_params = {param_name: np.random.normal(0, 10) for param_name in self._param_names}
+        if init_values is not None:
+            for key, value in init_values.items():
+                init_params[key] = value
+        init_dict = {'logits_array': init_diffs} | init_params
         lp = self._log_prob(time_series.disease_cases, time_series.mean_temperature)
         self._sample_key, key = jax.random.split(self._sample_key)
         self._param_samples = nuts_sample(lp, key, init_dict, 200, self._n_warmup_samples)
         last_params = {param_name: self._param_samples[param_name][-1] for param_name in self._param_samples}
         last_lp = lp(last_params)
         last_grad = jax.grad(lp)(last_params)
-        assert not any(np.any(np.isnan(value)) for value in last_grad.values()), (last_params, last_grad)
+        if any(np.any(np.isnan(value)) for value in last_grad.values()):
+            print(last_lp)
+            print(last_params)
+            print(last_grad)
+            for key, value in last_grad.items():
+                if np.any(np.isnan(value)):
+                    if np.ndim(self._param_samples[key]) == 1:
+                        plt.plot(self._param_samples[key])
+                        plt.title(key)
+                        plt.show()
+            lp(last_params)
+            raise ValueError('Nan in gradient')
         assert not np.isinf(last_lp), last_params
 
     def sample(self, climate_data: ClimateData) -> HealthData:
@@ -146,22 +163,91 @@ def test_mosquito_human_model():
 
 
 def test_mosquito_model():
-    return check_model_capacity(pure_mosquito_model, n_warmup_samples=1000).show()
+    """Simple mosquito model dependent on weather data.
+    Does not converge since depdending on paramters population can explode or die out,
+     where the probability of observing the scenario from one gets to be == 0 on the computer.
+     Need to restrict the parameter space sufficiently to ensure convergence.
+     """
+    return check_model_capacity(pure_mosquito_model, n_warmup_samples=10,
+                                data_filename='../../example_data/climate_data_daily.csv').show()
 
 
-def check_model_capacity(model, n_warmup_samples = 100):
+def test_investigate_carrying_capacity_function():
+    '''
+    Death rate is modelled as a function of the population size.
+    Need to find good parameters for that model. Trying to find
+    a parametrization that gives a cc of 1000.
+
+    a+bx = 0
+    a+b*1000 = 0
+    a = -1000b
+
+    Looks like alpha=-10, beta=0.01 seems good
+    '''
+    x = np.linspace(500, 1500, 100)
+    for beta in [0.0001, 0.001, 0.01, 0.1, 1, 10, 100]:
+        alpha = -1000 * beta
+        death_rates = get_death_rate(alpha, beta, x)
+        plt.plot(x, death_rates, label=f'alpha={alpha}, beta={beta}')
+    plt.legend()
+    plt.show()
+
+def test_investigate_temperature_dependency():
+    '''
+    Investigate the temperature dependency of the mosquito population.
+    We want the maturation rate to rize with temperature around 20-30 degrees.
+    alpha+beta*25 = 0
+    alpha = -beta*25
+    '''
+    x = np.linspace(0, 40, 100)
+    for beta in [0.0001, 0.001, 0.01, 0.1, 1, 10, 100]:
+        alpha = -25 * beta
+        maturation_rate = get_maturation_rate_by_temp(dict(temp_base=alpha, temp_dependency=beta), x)
+        plt.plot(x, maturation_rate, label=f'alpha={alpha}, beta={beta}')
+    plt.legend()
+    plt.show()
+
+def test_explore_parameter_space_for_mosquito():
+    """Set the parameters to reasonable values and check if we get a stable model.
+     This was possible with centrally parameterized model, so need to find a similar set of parameters for the non-central model.
+     """
+    (sample, log_prob, reconstruct_state), (param_names, n_states) = pure_mosquito_model()
+    climate_data = ClimateData.from_csv('../../example_data/climate_data_daily.csv')[:365*5]
+    real_params = {
+        'temp_base': -27.,
+        'temp_dependency': 1.,
+        'lo_pupae_maturation': logit(0.33),
+        'logscale': np.log(0.1),
+        'mosquito_death_logit': logit(0.1),
+        'carry_beta': 0.01, #Verified
+        'carry_alpha': -10, # Verified
+        'log_eggrate': jnp.log(10),
+        'lo_rate': 0
+    }
+
+    simulator = get_simulator(sample, real_params)
+    mosquito_data = simulator.simulate(climate_data)
+    print(mosquito_data.disease_cases)
+    plt.plot(mosquito_data.disease_cases)
+    plt.show()
+
+
+def check_model_capacity(model, n_warmup_samples=100, data_filename='../../example_data/climate_data.csv'):
     (sample, log_prob, reconstruct_state), (param_names, n_states) = model()
     if isinstance(param_names, dict):
         real_params = {name: np.random.normal(value, 3) for name, value in param_names.items()}
+        init_values = {name: np.random.normal(value, 3) for name, value in param_names.items()}
         param_names = list(param_names)
     else:
         real_params = {name: np.random.normal(0, 3) for name in param_names}
-    climate_data = ClimateData.from_csv('../../example_data/climate_data.csv')
+        init_values = None
+    climate_data = ClimateData.from_csv(data_filename)
     simulator = get_simulator(sample, real_params)
     health_data = simulator.simulate(climate_data)
-    sampler = SimpleSampler(jax.random.PRNGKey(0), log_prob, sample, param_names, n_states, n_warmup_samples=n_warmup_samples)
+    sampler = SimpleSampler(jax.random.PRNGKey(0), log_prob, sample, param_names, n_states,
+                            n_warmup_samples=n_warmup_samples)
     data_set = ClimateHealthTimeSeries.combine(health_data, climate_data)
-    sampler.train(data_set)
+    sampler.train(data_set, init_values=init_values)
     figure = prediction_plot(health_data, sampler, climate_data, 10)
     return figure
 
