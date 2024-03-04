09499ea31be9d61527eb25d24483dcdd031138ee
Knut Rand <knutdrand@gmail.com>
2024-03-04 14:29:55 +0100

    refactored out diffs from transition function

--- weekly_reports/modelling/week10.py
+++ weekly_reports/modelling/week10.py
@@ -7,28 +7,26 @@ from probabilistic_machine_learning.cases.diff_model import MosquitoModelSpec, _
 import jax.numpy as jnp
 from jax.scipy.special import expit, logit
 
+
 def _new_transition(P, logits, states):
     human_state, mosquito_state = states[..., :4], states[..., 4:]
     human_logits = logits[..., :4]
     human_diffs = human_state * expit(human_logits)
-    mosquito_death_rate = _get_mosquito_death_rate(mosquito_state, P)
-    death_rates = mosquito_death_rate
-    mosquito_state = tuple(s * (1 - expit(d)) for s, d in zip(mosquito_state.T, death_rates))
-    mosquito_logits = logits[..., 4:]
-    mosquito_diffs = tuple(ms * expit(lg) for ms, lg in zip(mosquito_state, mosquito_logits.T))
-    new_eggs = jnp.exp(P['log_eggrate']) * sum(mosquito_state[3:]) * expit(mosquito_logits.T[-1])
     full_human_diffs = -human_diffs + jnp.roll(human_diffs, 1, axis=-1)
     new_state = human_state + full_human_diffs
-    new_state = jnp.array(
-        [new_state[..., 0], new_state[..., 1], new_state[..., 2], new_state[..., 3],
-                 mosquito_state[0] - mosquito_diffs[0] + new_eggs,
-                 mosquito_state[1] - mosquito_diffs[1] + mosquito_diffs[0],
-                 mosquito_state[2] - mosquito_diffs[2] + mosquito_diffs[1],
-                 mosquito_state[3] - mosquito_diffs[3] + mosquito_diffs[2],
-                 mosquito_state[4] - mosquito_diffs[4] + mosquito_diffs[3],
-                 mosquito_state[5] + mosquito_diffs[4]]).T
-    return new_state, new_state
 
+    mosquito_death_rate = _get_mosquito_death_rate(mosquito_state, P)
+    death_rates = mosquito_death_rate
+    mosquito_deaths = tuple(s * expit(d) for s, d in zip(mosquito_state.T, death_rates))
+
+    mosquito_state_after_deaths = tuple(s - d for s, d in zip(mosquito_state.T, mosquito_deaths))
+    mosquito_logits = logits[..., 4:]
+    mosquito_diffs = tuple(ms * expit(lg) for ms, lg in zip(mosquito_state_after_deaths, mosquito_logits.T))
+    new_eggs = jnp.exp(P['log_eggrate']) * sum(mosquito_state_after_deaths[3:]) * expit(mosquito_logits.T[-1])
+    full_mosquito_diffs = [new_eggs-mosquito_diffs[0]]+[mosquito_diffs[i-1]-mosquito_diffs[i] for i in range(1, 5)]+[mosquito_diffs[4]]
+    new_mosquito_state = [a + b for a, b in zip(mosquito_state_after_deaths, full_mosquito_diffs)]
+    new_state = jnp.array([new_state[..., 0], new_state[..., 1], new_state[..., 2], new_state[..., 3]] + new_mosquito_state).T
+    return new_state, new_state
 
 
 class StateSpaceDiffMosquitoModelSpec(MosquitoModelSpec):
@@ -36,16 +34,17 @@ class StateSpaceDiffMosquitoModelSpec(MosquitoModelSpec):
         P = self._params
         return _new_transition(P, logits, states)
 
+
 def test_refactor_transition():
     '''Refactor the transition function to explicitly calculate the diffs and the new state.'''
+    T = 2
     old_spec = MosquitoModelSpec(MosquitoModelSpec.good_params)
     new_spec = StateSpaceDiffMosquitoModelSpec(MosquitoModelSpec.good_params)
-    states = jnp.array([old_spec.init_state]*5)
-    logits = jnp.linspace(-1, 1, 50).reshape(-1, 10)
+    states = jnp.array([old_spec.init_state] * T)
+    logits = jnp.linspace(-1, 1, T*10).reshape(-1, 10)
     old_result = old_spec.transition(states, logits)
     new_result = new_spec.transition(states, logits)
-    assert np.allclose(old_result[0], new_result[0]), (new_result[0:2], old_result[0:2])
-
+    np.testing.assert_allclose(old_result[0], new_result[0], rtol=1e-5)# , (new_result[0][0:2, :2], old_result[0][0:2, :2])
 
 
 def test_reparameterize_to_state_space_diffs():
@@ -53,6 +52,7 @@ def test_reparameterize_to_state_space_diffs():
     This makes the reconstruction step a simple cumsum and probably makes the autodiff more stable.
     Also move modelling code into climate_health repo to avoid discrepancies
     '''
+
     def sample_diffs(states, exogenous, params):
         human_state, mosquito_state = states[..., :4], states[..., 4:]
         human_logits = logits[..., :4]
