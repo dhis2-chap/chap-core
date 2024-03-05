980d102af00a35eef4974ef5b5b45ffe5d06f6cf
Knut Dagestad Rand <knutdrand@gmail.com>
2024-03-05 11:16:01 +0100

    Update test_model_building_acceptance.py

--- tests/test_model_building_acceptance.py
+++ tests/test_model_building_acceptance.py
@@ -5,12 +5,12 @@ from climate_health.datatypes import HealthData
 from climate_health.predictor.naive_predictor import NaiveForecastSampler
 from climate_health.time_period import Day
 from .mocks import ClimateDataBaseMock
-
+from omnipy import MultiModelDataset
 
 @pytest.mark.xfail
 def test_model_building_acceptance(health_data_set_filename: str, output_file_name: str):
     health_data = HealthData.from_csv(health_data_set_filename)
     data_set: SpatioTemporalDataSet = add_climate_data_to_health_data(health_data, ClimateDataBaseMock(), resolution=Day)
     model = NaiveForecastSampler
-    dashboard = evaluate_model_on_multiple_time_series(model, data_set)
-    dashboard.save(output_file_name)
+    results: MultiModelDataset = evaluate_model_on_multiple_time_series(model, data_set)
+    present_results(results)
