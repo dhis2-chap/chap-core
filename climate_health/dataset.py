a53f275e416ae087569752ce38c523e9eeef4ddc
Knut Dagestad Rand <knutdrand@gmail.com>
2024-03-05 11:01:52 +0100

    Update dataset.py

--- climate_health/dataset.py
+++ climate_health/dataset.py
@@ -1,4 +1,4 @@
-from typing import Protocol, Union, Iterable
+from typing import Protocol, Union, Iterable, TypeVar, Generic
 
 from climate_health.datatypes import Location
 from climate_health.time_period.dataclasses import Period
@@ -6,16 +6,16 @@ from climate_health.time_period.dataclasses import Period
 spatial_index_type = Union[str, Location]
 temporal_index_type = Union[Period, Iterable[Period], slice]
 
-
-class SpatioTemporalDataSet(Protocol):
+T = TypeVar('T')
+class SpatioTemporalDataSet(Protocol, Generic[T]):
     dataclass = ...
-    def get_locations(self, location: Iterable[Location])-> SpatioTemporalDataSet:
+    def get_locations(self, location: Iterable[Location])-> SpatioTemporalDataSet[T]:
         ...
 
-    def get_data_for_location(self, location: Location)->dataclass
+    def get_data_for_location(self, location: Location) -> T
         ...
         
-    def restrict_time_period(self, period_range: slice[Period]):
+    def restrict_time_period(self, period_range: slice[Period])->SpatioTemporalDataSet[T]:
         ...
         
     # def __getitem__(self, index: spatial_index_type|Tuple[spatial_index_type, temporal_index_type] ) -> Union['SpatioTemporalDataSet', 'dataclass']:
