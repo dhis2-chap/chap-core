8276d1ce364d81bed9277a42a66d1af8753ce6f7
Knut Rand <knutdrand@gmail.com>
2024-03-07 11:14:10 +0100

    feature: day_range

--- tests/time_period/test_period_range.py
+++ tests/time_period/test_period_range.py
@@ -11,10 +11,11 @@ def test_period_range():
     true_range = Month(month=[7, 8, 9, 10, 11, 0, 1, 2], year=[2020, 2020, 2020, 2020, 2020, 2021, 2021, 2021])
     assert_bnpdataclass_equal(period_range(start, end), true_range)
 
-@pytest.mark.xfail
+
 def test_period_range_day():
-    start = Day.single_entry(2020, 1, 28)
-    end = Day.single_entry(2020, 2, 1)
+    start = Day.single_entry(2020, 0, 28)
+    end = Day.single_entry(2020, 1, 1)
     days = [28, 29, 30, 0, 1]
-    true_range = Day([2020]*len(days), [1, 1, 1, 2, 2], days)
-    assert_bnpdataclass_equal(period_range(start, end), true_range)
+    true_range = Day([2020]*len(days), [0, 0, 0, 1, 1], days)
+    result = period_range(start, end)
+    assert_bnpdataclass_equal(result, true_range)
