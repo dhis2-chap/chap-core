86c6e3415709a35a0b3f9479037e38e0356f4c6f
Charlotte-Wurtzen <charlotte.wurtzen@gmail.com>
2024-03-08 08:53:31 +0100

    adding time period to x-axis in forecast plot

--- climate_health/plotting/prediction_plot.py
+++ climate_health/plotting/prediction_plot.py
@@ -1,6 +1,9 @@
 import numpy as np
+import pandas as pd
 from matplotlib import pyplot as plt
+import plotly.express as px
 from plotly.graph_objs import Figure
+import plotly.graph_objects as go
 
 from climate_health.datatypes import ClimateData, HealthData
 from climate_health.predictor.protocol import Sampler
@@ -18,15 +21,25 @@ def prediction_plot(true_data: HealthData, predicition_sampler: Sampler, climate
     plt.title('Prdicted path using estimated parameters vs real path')
     return plt.gcf()
 
+
 def forecast_plot(true_data: HealthData, predicition_sampler: Sampler, climate_data: ClimateData, n_samples) -> Figure:
     samples = np.array([predicition_sampler.sample(climate_data) for _ in range(n_samples)])
     quantiles = np.quantile(samples, [0.1, 0.5, 0.9], axis=0)
-    plt.plot(quantiles[1], label='50th percentile', color='grey')
-    x = np.arange(len(true_data.disease_cases))
-    plt.fill_between(x, quantiles[0], quantiles[2], color='grey', alpha=0.5)
-    plt.plot(quantiles[0], label='10th percentile', color='grey')
-    plt.plot(quantiles[2], label='90th percentile', color='grey')
-    plt.plot(true_data.disease_cases, label='real', color='blue')
-    plt.legend()
-    plt.title('Prdicted path using estimated parameters vs real path')
-    return plt.gcf()
+    x = ["-".join([str(x.year), str(x.month+1).zfill(2)]) for x in true_data.time_period]
+    df = pd.DataFrame({'x': x, '10th': quantiles[0], '50th': quantiles[1], '90th': quantiles[2]})
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(x=df["x"], y=df["10th"], mode="lines", line=dict(color='lightgrey'), fill=None, name="10th percentile"))
+    fig.add_trace(go.Scatter(x=df["x"], y=df["50th"], mode="lines", line=dict(color='grey'), fill="tonexty", name="50th percentile"))
+    fig.add_trace(go.Scatter(x=df["x"], y=df["90th"], mode="lines", line=dict(color='black'), fill="tonexty", name="90th percentile"))
+
+    fig.add_scatter(x=x, y=true_data.disease_cases, mode='lines', name='real', line=dict(color='blue'))
+
+    fig.update_layout(
+        title="Predicted path using estimated parameters vs real path",
+        xaxis_title="Time Period",
+        yaxis_title="Disease Cases")
+
+    return fig
+
