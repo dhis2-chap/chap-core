2775ce4a8caa567269b7be7e89c3d0aba62a5135
Knut Rand <knutdrand@gmail.com>
2024-03-11 20:22:07 +0100

    test: skip nominatim test if object creation fails

--- tests/test_location_lookup.py
+++ tests/test_location_lookup.py
@@ -2,6 +2,7 @@ from climate_health.datatypes import Location
 from climate_health.geo_coding.location_lookup import LocationLookup
 import pytest
 
+
 def test_location_lookup_add_location():
     location_lookup = LocationLookup()
     location_lookup.add_location('Oslo')
@@ -10,6 +11,7 @@ def test_location_lookup_add_location():
     assert 'Paris' in location_lookup
     assert 'MadeUpLocation' not in location_lookup
 
+
 # @pytest.mark.xfail
 def test_location_lookup_contains_arcgis():
     location_lookup = LocationLookup('ArcGIS')
@@ -17,12 +19,14 @@ def test_location_lookup_contains_arcgis():
     assert 'Paris' in location_lookup
     assert 'MadeUpLocation' not in location_lookup
 
+
 # @pytest.mark.xfail
 def test_location_lookup_contains_noninatime():
     location_lookup = LocationLookup()
     assert 'Oslo' in location_lookup
     assert 'Paris' in location_lookup
     assert 'MadeUpLocation' not in location_lookup
+
 
 # @pytest.mark.xfail
 def test_location_lookup_same_city():
@@ -42,6 +46,7 @@ def test_location_lookup_getitem_arcgis():
                                                10.75)
     assert location_lookup['Paris'] == Location(48.863697576,
                                                 2.361657337)
+
 
 @pytest.mark.xfail
 def test_location_lookup_getitem_noninatime():
@@ -65,9 +70,20 @@ def test_arcgis_geolocator():
     location_lookup = LocationLookup('ArcGIS')
     assert location_lookup.geolocator.__class__.__name__ == 'ArcGIS'
 
+@pytest.fixture()
+def nominatim_lookup():
+    try:
+        lookup = LocationLookup('Nominatim')
+    except:
+        pytest.skip()
+    return lookup
 
-def test_nominatim_geolocator():
-    location_lookup = LocationLookup('Nominatim')
+
+
+
+def test_nominatim_geolocator(nominatim_lookup):
+    location_lookup = nominatim_lookup
+    #location_lookup = LocationLookup('Nominatim')
     assert location_lookup.geolocator.__class__.__name__ == 'Nominatim'
 
 
@@ -76,7 +92,9 @@ def test_print_location_lookup_arcgis():
     location_lookup.add_location('Oslo')
     location_lookup.add_location('Paris')
     print(location_lookup)
-    assert str(location_lookup) == '{\'Oslo\': Location(Oslo, (59.91234, 10.75, 0.0)), \'Paris\': Location(Paris, Île-de-France, (48.863697576, 2.361657337, 0.0))}'
+    assert str(
+        location_lookup) == '{\'Oslo\': Location(Oslo, (59.91234, 10.75, 0.0)), \'Paris\': Location(Paris, Île-de-France, (48.863697576, 2.361657337, 0.0))}'
+
 
 @pytest.mark.xfail
 def test_print_location_lookup_noninatime():
@@ -84,4 +102,5 @@ def test_print_location_lookup_noninatime():
     location_lookup.add_location('Oslo')
     location_lookup.add_location('Paris')
     print(location_lookup)
-    assert str(location_lookup) == '{\'Oslo\': Location(Oslo, Norge, (59.9133301, 10.7389701, 0.0)), \'Paris\': Location(Paris, Île-de-France, France métropolitaine, France, (48.8534951, 2.3483915, 0.0))}'
+    assert str(
+        location_lookup) == '{\'Oslo\': Location(Oslo, Norge, (59.9133301, 10.7389701, 0.0)), \'Paris\': Location(Paris, Île-de-France, France métropolitaine, France, (48.8534951, 2.3483915, 0.0))}'
