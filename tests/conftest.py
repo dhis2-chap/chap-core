cf05e1e108dce03534a4aa45c541419d1d2a0513
mmamica <marimam@uio.no>
2024-03-15 11:22:21 +0100

    feat: divide cache into prod and test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 import pytest
@@ -6,3 +7,10 @@ import pytest
 @pytest.fixture
 def data_path():
     return Path(__file__).parent.parent / 'example_data'
+
+
+@pytest.fixture(scope="session", autouse=True)
+def use_test_cache():
+    os.environ['TEST_ENV'] = 'true'
+    yield
+    del os.environ['TEST_ENV']
