5f00d6e3b20376fd3e2a7b1618ec1abef0a8895e
Charlotte-Wurtzen <charlotte.wurtzen@gmail.com>
2024-03-15 16:16:59 +0100

    fix timeperiod in data_fixtures for multi-loc-evaluator test

--- tests/single_assessment/test_multi_location_evaluator.py
+++ tests/single_assessment/test_multi_location_evaluator.py
@@ -1,6 +1,8 @@
-from climate_health.assessment.multi_location_evaluator import MultiLocationEvaluator
-from ..data_fixtures import full_data, train_data, future_climate_data, good_predictions, bad_predictions
 import pytest
+
+from climate_health.assessment.multi_location_evaluator import MultiLocationEvaluator
+from ..data_fixtures import full_data, good_predictions, bad_predictions
+
 
 @pytest.mark.xfail
 def test_multi_location_evaluator(full_data, good_predictions, bad_predictions):
