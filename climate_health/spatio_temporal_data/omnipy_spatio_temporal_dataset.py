c66a771a9c26bbbfeb45f376fbb4124bfd747a09
Sveinung Gundersen <sveinugu@gmail.com>
2024-03-20 18:13:19 +0100

    Adding multi-resolution support in data hierarchy. Explicitly wrap all Pydantic models as Omnipy models

--- climate_health/spatio_temporal_data/omnipy_spatio_temporal_dataset.py
+++ climate_health/spatio_temporal_data/omnipy_spatio_temporal_dataset.py
@@ -3,11 +3,12 @@ from typing import Generic, TypeVar
 
 from omnipy import Dataset, Model
 from omnipy.modules.json.models import JsonListM
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, root_validator, validator
+from pydantic.generics import GenericModel
 
 
-class TemporalDataModel(BaseModel):
-    start_date: str | date
+class TemporalDataPydanticModel(BaseModel):
+    start_date: str | date = date.fromisoformat("1970-01-01")
 
     @validator("start_date")
     def parse_start_date(cls, data: str | date) -> date:
@@ -17,24 +18,45 @@ class TemporalDataModel(BaseModel):
             return data
 
 
-TemporalDataModelT = TypeVar("TemporalDataModelT", bound=TemporalDataModel)
+TemporalDataPydanticModelT = TypeVar(
+    "TemporalDataPydanticModelT", bound=TemporalDataPydanticModel
+)
 
 
 class TemporalDataOmnipyModel(
-    Model[JsonListM[TemporalDataModelT]], Generic[TemporalDataModelT]
+    Model[TemporalDataPydanticModelT], Generic[TemporalDataPydanticModelT]
 ): ...
 
 
-TemporalDataPydanticModelT = TypeVar("TemporalDataPydanticModelT", bound=BaseModel)
+class MultiResolutionTemporalDataPydanticModel(
+    GenericModel, Generic[TemporalDataPydanticModelT]
+):
+    days: Model[list[TemporalDataOmnipyModel[TemporalDataPydanticModelT]]] = []
+    weeks: Model[list[TemporalDataOmnipyModel[TemporalDataPydanticModelT]]] = []
+    months: Model[list[TemporalDataOmnipyModel[TemporalDataPydanticModelT]]] = []
+    inconsistent: Model[list[TemporalDataOmnipyModel[TemporalDataPydanticModelT]]] = []
+
+
+class MultiResolutionTemporalDataOmnipyModel(
+    Model[MultiResolutionTemporalDataPydanticModel[TemporalDataPydanticModelT]],
+    Generic[TemporalDataPydanticModelT],
+): ...
+
+
+class SpatioTemporalDataPydanticModel(BaseModel):
+    @root_validator
+    def check_all_field_types_are_temporal_omnipy_models(cls, values):
+        for field in cls.__fields__.values():
+            assert issubclass(field.type_, MultiResolutionTemporalDataOmnipyModel)
+        return values
+
+
+SpatioTemporalDataPydanticModelT = TypeVar(
+    "SpatioTemporalDataPydanticModelT", bound=SpatioTemporalDataPydanticModel
+)
 
 
 class SpatioTemporalDataOmnipyDataset(
-    Dataset[Model[TemporalDataPydanticModelT]],
-    Generic[TemporalDataPydanticModelT],
-):
-    def __new__(cls, *args, **kwargs):
-        pydantic_model = cls.get_model_class().outer_type()
-        for field in pydantic_model.__fields__.values():
-            assert issubclass(field.type_, TemporalDataOmnipyModel)
-
-        return super().__new__(cls)
+    Dataset[Model[SpatioTemporalDataPydanticModelT]],
+    Generic[SpatioTemporalDataPydanticModelT],
+): ...
--- tests/spatio_temporal_data/test_omnipy_spatio_temporal_dataset.py
+++ tests/spatio_temporal_data/test_omnipy_spatio_temporal_dataset.py
@@ -1,3 +1,4 @@
+import datetime
 from typing import Annotated, TypeAlias
 
 import pytest
@@ -8,81 +9,108 @@ from pydantic import BaseModel
 
 from climate_health.spatio_temporal_data.omnipy_spatio_temporal_dataset import (
     SpatioTemporalDataOmnipyDataset,
-    TemporalDataModel,
-    TemporalDataOmnipyModel,
+    TemporalDataPydanticModel,
+    SpatioTemporalDataPydanticModel,
+    MultiResolutionTemporalDataOmnipyModel,
 )
 
-JsonTestDataType: TypeAlias = dict[str, dict[str, list[dict[str, JsonScalar]]]]
+JsonTestDataType: TypeAlias = dict[
+    str, dict[str, dict[str, list[dict[str, JsonScalar]]]]
+]
 
 
 @pytest.fixture
 def simple_test_data() -> Annotated[JsonTestDataType, pytest.fixture]:
     return dict(
         region_1=dict(
-            disease=[
-                dict(start_date="2024-01-01", count=41),
-                dict(start_date="2024-01-08", count=30),
-                dict(start_date="2024-01-15", count=23),
-                dict(start_date="2024-01-22", count=38),
-                dict(start_date="2024-01-29", count=24),
-                dict(start_date="2024-02-05", count=19),
-                dict(start_date="2024-02-12", count=31),
-                dict(start_date="2024-02-19", count=35),
-                dict(start_date="2024-02-26", count=32),
-            ],
-            weather=[
-                dict(
-                    start_date="2024-01-01",
-                    rain=12.5,
-                    category="low",
-                ),
-                dict(
-                    start_date="2024-02-01",
-                    rain=24.3,
-                    category="high",
-                ),
-            ],
+            disease=dict(
+                weeks=[
+                    dict(start_date="2024-01-01", count=41),
+                    dict(start_date="2024-01-08", count=30),
+                    dict(start_date="2024-01-15", count=23),
+                    dict(start_date="2024-01-22", count=38),
+                    dict(start_date="2024-01-29", count=24),
+                    dict(start_date="2024-02-05", count=19),
+                    dict(start_date="2024-02-12", count=31),
+                    dict(start_date="2024-02-19", count=35),
+                    dict(start_date="2024-02-26", count=32),
+                ]
+            ),
+            weather=dict(
+                months=[
+                    dict(
+                        start_date="2024-01-01",
+                        rain=12.5,
+                        category="low",
+                    ),
+                    dict(
+                        start_date="2024-02-01",
+                        rain=24.3,
+                        category="high",
+                    ),
+                ]
+            ),
         ),
         region_2=dict(
-            disease=[
-                dict(
-                    start_date=f"2024-{(i // 31) + 1:02d}-{(i % 31) + 1:02d}",
-                    count=(i % 5) + 1,
-                )
-                for i in range(60)
-            ],
-            weather=[
-                dict(
-                    start_date="2024-01-01",
-                    rain=18.3,
-                    category="medium",
-                ),
-                dict(
-                    start_date="2024-02-01",
-                    rain=19.2,
-                    category="medium",
-                ),
-            ],
+            disease=dict(
+                days=[
+                    dict(
+                        start_date=f"2024-{(i // 31) + 1:02d}-{(i % 31) + 1:02d}",
+                        count=(i % 5) + 1,
+                    )
+                    for i in range(60)
+                ]
+            ),
+            weather=dict(
+                months=[
+                    dict(
+                        start_date="2024-01-01",
+                        rain=18.3,
+                        category="medium",
+                    ),
+                    dict(
+                        start_date="2024-02-01",
+                        rain=19.2,
+                        category="medium",
+                    ),
+                ]
+            ),
         ),
     )
 
 
-class DiseaseFeatures(TemporalDataModel):
+class DiseaseFeatures(TemporalDataPydanticModel):
     count: int
 
 
-class ClimateFeatures(TemporalDataModel):
+class ClimateFeatures(TemporalDataPydanticModel):
     rain: float
     category: str
 
 
-class MyTemporalDataModel(BaseModel):
-    disease: TemporalDataOmnipyModel[DiseaseFeatures]
-    weather: TemporalDataOmnipyModel[ClimateFeatures]
+class MyTemporalDataModel(SpatioTemporalDataPydanticModel):
+    disease: MultiResolutionTemporalDataOmnipyModel[DiseaseFeatures]
+    weather: MultiResolutionTemporalDataOmnipyModel[ClimateFeatures]
 
 
 def test_spatio_temporal_dataset(
     simple_test_data: Annotated[JsonTestDataType, pytest.fixture]
 ):
     dataset = SpatioTemporalDataOmnipyDataset[MyTemporalDataModel](simple_test_data)
+
     assert len(dataset) == 2
+    for region, spatio_temp_data in dataset.items():
+        # Use of inner data objects are currently inconsistent with the outer dataset (which uses dict syntax) due to
+        # switching between Omnipy datasets and Pydantic models. Will be harmonised in a newer version of Omnipy
+        assert hasattr(spatio_temp_data, "disease")
+        assert hasattr(spatio_temp_data, "weather")
+
+        for category, multi_res_temp_data in spatio_temp_data:
+            assert hasattr(multi_res_temp_data, "days")
+            assert hasattr(multi_res_temp_data, "weeks")
+            assert hasattr(multi_res_temp_data, "months")
+            assert hasattr(multi_res_temp_data, "inconsistent")
+
+            for resolution, temp_data in multi_res_temp_data:
+                for record in temp_data:
+                    assert isinstance(record.start_date, datetime.date)
