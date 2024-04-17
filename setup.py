66e3ac4025872d7abffb079a20917b617b6e5a6a
Knut Rand <knutdrand@gmail.com>
2024-04-17 14:13:15 +0200

    fix: missing dep

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = ['typer', 'numpy', 'bionumpy', 'pandas',  'plotly', 'scikit-learn', 'matplotlib', 'diskcache', 'geopy', 'pooch',
-                'python-dateutil', 'meteostat', 'cyclopts', 'requests','pydantic']
+                'python-dateutil', 'meteostat', 'cyclopts', 'requests','pydantic', 'yaml']
 
 test_requirements = ['pytest>=3', "hypothesis"]
 
