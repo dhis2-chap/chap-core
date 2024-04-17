f845a68422fee0e43d695d1cb68f1cb59da783a0
Knut Rand <knutdrand@gmail.com>
2024-04-17 13:51:47 +0200

    fix: remove jax from imports

--- climate_health/external/models/jax_models/jax.py
+++ climate_health/external/models/jax_models/jax.py
@@ -5,5 +5,7 @@ try:
     import jax.numpy as jnp
     import jax
     import blackjax
+    from jax.scipy.special import expit, logit
 except ImportError as e:
     jax, jnp, stats, PRNGKey, blackjax = (None, None, None, None, None)
+    expit, logit = (None, None)
