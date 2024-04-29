347eb00a78e3249534d2e5846aa0d715933a5de9
Ivar Grytten <ivar.grytten@gmail.com>
2024-04-29 14:52:10 +0200

    feature: Working Dockerfile for installing R and INLA

--- external_models/ewars_Plus/setup.R
+++ external_models/ewars_Plus/setup.R
@@ -1,4 +1,5 @@
 r = getOption("repos")
 r["CRAN"] = "http://cran.us.r-project.org"
 options(repos = r)
+install.packages("terra")
 install.packages("INLA", repos=c(getOption("repos"), INLA="https://inla.r-inla-download.org/R/testing"), dep=TRUE)
