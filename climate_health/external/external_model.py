7e1dbdc4b199c6f70195076b1295b9174e7553b8
Ivar Grytten <ivar.grytten@gmail.com>
2024-05-29 14:28:27 +0200

    feature: initing models from path or github with working dir as tmp path

--- climate_health/external/external_model.py
+++ climate_health/external/external_model.py
@@ -71,7 +71,7 @@ class ExternalCommandLineModel(Generic[FeatureType]):
         return self
 
     @classmethod
-    def from_yaml_file(cls, yaml_file: str, dockername: Optional[str] = None) -> "ExternalCommandLineModel":
+    def from_yaml_file(cls, yaml_file: str, working_dir, dockername: Optional[str] = None) -> "ExternalCommandLineModel":
         # read yaml file into a dict
         with open(yaml_file, 'r') as file:
             data = yaml.load(file, Loader=yaml.FullLoader)
@@ -92,7 +92,8 @@ class ExternalCommandLineModel(Generic[FeatureType]):
             predict_command=predict_command,
             data_type=data_type,
             setup_command=setup_command,
-            working_dir=Path(yaml_file).parent,
+            working_dir=working_dir,
+            #working_dir=Path(yaml_file).parent,
             adapters=data.get('adapters', None),
             runner=runner,
         )
@@ -279,8 +280,8 @@ class SimpleFileContextManager:
             return self.file.read()
 
 
-def get_model_from_yaml_file(yaml_file: str, dockername: Optional[str] = None) -> ExternalCommandLineModel:
-    return ExternalCommandLineModel.from_yaml_file(yaml_file, dockername)
+def get_model_from_yaml_file(yaml_file: str, working_dir, dockername: Optional[str] = None) -> ExternalCommandLineModel:
+    return ExternalCommandLineModel.from_yaml_file(yaml_file, working_dir, dockername)
 
 
 def get_runner_from_yaml_file(yaml_file: str, dockername: Optional[str] = None) -> Runner:
