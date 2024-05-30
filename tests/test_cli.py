62c0e3674d1cde555439cf3b1ac6cf5f3c151904
Ivar Grytten <ivar.grytten@gmail.com>
2024-05-30 15:33:20 +0200

    fix: testskip

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,7 +3,7 @@ import pytest
 from climate_health.util import docker_available
 
 
-@pytest.mark.skipif(not docker_available())
+@pytest.mark.skipif(not docker_available(), reason="Docker not available")
 def test_forecast_github_model():
     repo_url = "https://github.com/knutdrand/external_rmodel_example.git"
     results = forecast("external", "hydromet_5_filtered", 12, repo_url)
