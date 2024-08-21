7adda376bd84d0bd9f06013fd718ecd58bb5d473
Herman Tretteteig <hermat@uio.no>
2024-08-21 12:44:20 +0200

    Redis without exposing port

--- climate_health/worker/rq_worker.py
+++ climate_health/worker/rq_worker.py
@@ -7,6 +7,8 @@ from typing import Callable, Generic
 from rq import Queue
 from rq.job import Job
 from redis import Redis
+import os
+from dotenv import load_dotenv, find_dotenv
 
 from climate_health.worker.interface import ReturnType
 import logging
@@ -43,7 +45,21 @@ class RedisJob(Generic[ReturnType]):
 
 class RedisQueue:
     def __init__(self):
-        self.q = Queue(connection=Redis())
+        host, port = self.read_environment_variables()
+        self.q = Queue(connection=Redis(host=host, port=int(port)))
+
+    def read_environment_variables(self):
+        load_dotenv(find_dotenv())
+        host = os.environ.get('REDIS_HOST')
+        port = os.environ.get('REDIS_PORT')
+
+        #using default values if environment variables are not set
+        if host is None:
+            host = 'localhost'
+        if port is None:
+            port = '6379'
+
+        return host, port
 
     def queue(self, func: Callable[..., ReturnType], *args, **kwargs) -> RedisJob[ReturnType]:
         return RedisJob(self.q.enqueue(func, *args, **kwargs))
