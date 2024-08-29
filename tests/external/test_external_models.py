20806122d437036df315a494d4fa912f4b8ee74c
Ivar Grytten <ivar.grytten@gmail.com>
2024-08-29 14:38:54 +0200

    getting test external models with ewars to work

--- tests/external/test_external_models.py
+++ tests/external/test_external_models.py
@@ -9,11 +9,11 @@ import yaml
 
 from climate_health.api import get_model_from_directory_or_github_url
 from climate_health.spatio_temporal_data.temporal_dataclass import SpatioTemporalDict
-from climate_health.datatypes import ClimateHealthTimeSeries
+from climate_health.datatypes import ClimateHealthTimeSeries, FullData
 
 logging.basicConfig(level=logging.INFO)
 from climate_health.external.external_model import get_model_from_yaml_file, run_command
-from ..data_fixtures import full_data, train_data, future_climate_data
+from ..data_fixtures import full_data, train_data, train_data_pop, future_climate_data
 from climate_health.util import conda_available
 
 
@@ -35,7 +35,7 @@ def test_python_model_from_folder(models_path, train_data, future_climate_data):
     assert results is not None
 
 
-def get_dataset_from_yaml(yaml_path: Path):
+def get_dataset_from_yaml(yaml_path: Path, datatype=ClimateHealthTimeSeries):
     specs = yaml.load(yaml_path.read_text(), Loader=yaml.FullLoader)
     if 'demo_data' in specs:
         path = yaml_path.parent / specs['demo_data']
@@ -49,19 +49,19 @@ def get_dataset_from_yaml(yaml_path: Path):
                 df[to_name] = df[from_name]
     #df['disease_cases'] = np.arange(len(df))
 
-    return SpatioTemporalDict.from_pandas(df, ClimateHealthTimeSeries)
+    return SpatioTemporalDict.from_pandas(df, datatype)
 
 
 #@pytest.mark.skipif(not conda_available(), reason='requires conda')
 @pytest.mark.parametrize('model_directory', ['ewars_Plus'])
 #@pytest.mark.parametrize('model_directory', ['naive_python_model'])
-def test_all_external_models_acceptance(model_directory, models_path, train_data, future_climate_data):
+def test_all_external_models_acceptance(model_directory, models_path, train_data_pop, future_climate_data):
     """Only tests that the model can be initiated and that train and predict
     can be called without anything failing"""
     print("Running")
     yaml_path = models_path / model_directory / 'config.yml'
     model = get_model_from_yaml_file(yaml_path, working_dir=models_path / model_directory)
-    train_data = get_dataset_from_yaml(yaml_path)
+    train_data = get_dataset_from_yaml(yaml_path, FullData)
     model.setup()
     model.train(train_data)
     #results = model.predict(future_climate_data)
@@ -72,11 +72,12 @@ def test_all_external_models_acceptance(model_directory, models_path, train_data
 @pytest.mark.parametrize('model_directory', ['ewars_Plus'])
 def test_external_model_predict(model_directory, models_path):
     yaml_path = models_path / model_directory / 'config.yml'
-    train_data = get_dataset_from_yaml(yaml_path)
+    train_data = get_dataset_from_yaml(yaml_path, FullData)
     model = get_model_from_yaml_file(yaml_path, working_dir=models_path / model_directory)
     model.setup()
     #model.setup()
     results = model.predict(train_data)
+    print(results)
     assert isinstance(results, SpatioTemporalDict)
 
 
