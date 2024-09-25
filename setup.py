66dc35bba22201aa11956b939828d345520dda26
Ivar Grytten <ivar.grytten@gmail.com>
2024-09-25 18:27:29 +0200

    testcommit for pip

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MITupdate proje License",
+        'License :: OSI Approved :: MIT License',
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
