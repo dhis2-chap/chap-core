c606f984fa4052503e743b8ceae10c5c6bb3a44e
Ivar Grytten <ivar.grytten@gmail.com>
2025-04-03 11:38:20 +0200

    ruff

--- chap_core/external/mlflow_wrappers.py
+++ chap_core/external/mlflow_wrappers.py
@@ -1,8 +1,4 @@
 # this file can be removed in the future, code has moved
-from typing import Optional, TypeVar
-import logging
-logger = logging.getLogger(__name__)
-FeatureType = TypeVar("FeatureType")
 
 
 
