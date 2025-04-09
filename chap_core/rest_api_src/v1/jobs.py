02d8c087e9dc7860e1ea3d70563d86ea567fe9b9
Karim Bahgat <karimba@uio.no>
2025-04-09 15:32:58 +0200

    fix: api job logs error

--- chap_core/rest_api_src/v1/jobs.py
+++ chap_core/rest_api_src/v1/jobs.py
@@ -19,26 +19,25 @@ def list_jobs() -> List[JobDescription]:
     return worker.list_jobs()
 
 
-def get_job(job_id):
+def get_successful_job(job_id):
     job = worker.get_job(job_id)
-    print(job_id, job.status, job.result)
     if job.status.lower() == "failed":
         raise HTTPException(status_code=400, detail="Job failed. Check the exception endpoint for more information")
 
     if not (job and job.is_finished):
-        raise HTTPException(status_code=400, detail="No response available")
+        raise HTTPException(status_code=400, detail="Job is still running, try again later")
 
     return job
 
 
 @router.get("/{job_id}")
 def get_job_status(job_id: str) -> str:
-    return get_job(job_id).status
+    return worker.get_job(job_id).status
 
 
 @router.get("/{job_id}/logs")
 def get_logs(job_id: str) -> str:
-    job = get_job(job_id)
+    job = worker.get_job(job_id)
     logs = job.get_logs()
     if logs is None:
         raise HTTPException(status_code=400, detail=f"Log file not found for job ID '{job_id}'")
@@ -47,12 +46,12 @@ def get_logs(job_id: str) -> str:
 
 @router.get("/{job_id}/prediction_result")
 def get_prediction_result(job_id: str) -> FullPredictionResponse:
-    return get_job(job_id).result
+    return get_successful_job(job_id).result
 
 
 @router.get("/{job_id}/evaluation_result")
 def get_evaluation_result(job_id: str) -> EvaluationResponse:
-    return get_job(job_id).result
+    return get_successful_job(job_id).result
 
 
 class DataBaseResponse(BaseModel):
@@ -61,7 +60,7 @@ class DataBaseResponse(BaseModel):
 
 @router.get("/{job_id}/database_result")
 def get_database_result(job_id: str) -> DataBaseResponse:
-    result = get_job(job_id).result
+    result = get_successful_job(job_id).result
     return DataBaseResponse(id=result)
 
 
