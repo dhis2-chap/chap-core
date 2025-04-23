f926823fef57c2c05bb7ab65af714fe932b9b868
Ivar Grytten <ivar.grytten@gmail.com>
2025-04-23 12:36:40 +0200

    ruff

--- chap_core/adaptors/command_line_interface.py
+++ chap_core/adaptors/command_line_interface.py
@@ -1,7 +1,6 @@
 from cyclopts import App
 
 from chap_core.datatypes import remove_field, create_tsdataclass
-from chap_core.external.model_configuration import ModelTemplateConfig, EntryPointConfig, CommandConfig
 from chap_core.model_spec import get_dataclass
 from chap_core.models import ModelTemplate
 from chap_core.spatio_temporal_data.temporal_dataclass import DataSet
