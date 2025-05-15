e3323d0f9dbccfd4a840c8f50f2be5de006b2da6
eirikhaugstulen <eirik.haugstulen@gmail.com>
2025-05-16 00:07:39 +0200

    refactor: revert styling

--- chap_core/rest_api_src/db_worker_functions.py
+++ chap_core/rest_api_src/db_worker_functions.py
@@ -7,12 +7,7 @@ from chap_core.datatypes import FullData, HealthPopulationData, create_tsdatacla
 from chap_core.predictor.model_registry import registry
 from chap_core.assessment.prediction_evaluator import backtest as _backtest
 from chap_core.rest_api_src.data_models import FetchRequest, BackTestCreate
-
-# from chap_core.rest_api_src.v1.routers.crud import BackTestCreate
-from chap_core.rest_api_src.worker_functions import (
-    harmonize_health_dataset,
-    WorkerConfig,
-)
+from chap_core.rest_api_src.worker_functions import harmonize_health_dataset, WorkerConfig
 from chap_core.data import DataSet as InMemoryDataSet
 from chap_core.time_period import Month
 
@@ -21,54 +16,38 @@ def trigger_exception(*args, **kwargs):
     raise Exception("Triggered exception")
 
 
-def run_backtest(
-    info: BackTestCreate,
-    n_periods: Optional[int] = None,
-    n_splits: int = 10,
-    stride: int = 1,
-    session: SessionWrapper = None,
-):
+def run_backtest(info: BackTestCreate,
+                 n_periods: Optional[int] = None,
+                 n_splits: int = 10,
+                 stride: int = 1,
+                 session: SessionWrapper = None):
     dataset = session.get_dataset(info.dataset_id, FullData)
     if n_periods is None:
         n_periods = _get_n_periods(dataset)
-    estimator = registry.get_model(
-        info.model_id, ignore_env=info.model_id.startswith("chap_ewars")
-    )
-    predictions_list = _backtest(
-        estimator,
-        dataset,
-        prediction_length=n_periods,
-        n_test_sets=n_splits,
-        stride=stride,
-        weather_provider=QuickForecastFetcher,
-    )
+    estimator = registry.get_model(info.model_id, ignore_env=info.model_id.startswith('chap_ewars'))
+    predictions_list = _backtest(estimator,
+                                 dataset,
+                                 prediction_length=n_periods,
+                                 n_test_sets=n_splits,
+                                 stride=stride,
+                                 weather_provider=QuickForecastFetcher)
     last_train_period = dataset.period_range[-1]
     db_id = session.add_evaluation_results(predictions_list, last_train_period, info)
     assert db_id is not None
     return db_id
 
 
-def run_prediction(
-    estimator_id: registry.model_type,
-    dataset_id: str,
-    n_periods: Optional[int],
-    name: str,
-    metadata: dict,
-    session: SessionWrapper,
-):
+def run_prediction(estimator_id: registry.model_type, dataset_id: str, n_periods: Optional[int], name: str,
+                   metadata: dict, session: SessionWrapper):
     dataset = session.get_dataset(dataset_id, FullData)
     if n_periods is None:
         n_periods = _get_n_periods(dataset)
 
     # Get the configured model from the session_wrapper
     # session.get_configured_model(estimator_id)
-    estimator = registry.get_model(
-        estimator_id, ignore_env=estimator_id.startswith("chap_ewars")
-    )
+    estimator = registry.get_model(estimator_id, ignore_env=estimator_id.startswith('chap_ewars'))
     predictions = forecast_ahead(estimator, dataset, n_periods)
-    db_id = session.add_predictions(
-        predictions, dataset_id, estimator_id, name, metadata
-    )
+    db_id = session.add_predictions(predictions, dataset_id, estimator_id, name, metadata)
     assert db_id is not None
     return db_id
 
@@ -77,19 +56,11 @@ def debug(session: SessionWrapper):
     return session.add_debug()
 
 
-def harmonize_and_add_health_dataset(
-    health_dataset: FullData,
-    name: str,
-    session: SessionWrapper,
-    worker_config=WorkerConfig(),
-) -> FullData:
+def harmonize_and_add_health_dataset(health_dataset: FullData, name: str, session: SessionWrapper,
+                                     worker_config=WorkerConfig()) -> FullData:
     health_dataset = InMemoryDataSet.from_dict(health_dataset, HealthPopulationData)
-    dataset = harmonize_health_dataset(
-        health_dataset, usecwd_for_credentials=False, worker_config=worker_config
-    )
-    db_id = session.add_dataset(
-        name, dataset, polygons=health_dataset.polygons.model_dump_json()
-    )
+    dataset = harmonize_health_dataset(health_dataset, usecwd_for_credentials=False, worker_config=worker_config)
+    db_id = session.add_dataset(name, dataset, polygons=health_dataset.polygons.model_dump_json())
     return db_id
 
 
@@ -113,18 +84,18 @@ def harmonize_and_add_dataset(
         )
     else:
         full_dataset = health_dataset
-    db_id = session.add_dataset(
-        name,
-        full_dataset,
-        polygons=health_dataset.polygons.model_dump_json(),
-        dataset_type=ds_type,
-    )
+        db_id = session.add_dataset(
+            name,
+            full_dataset,
+            polygons=health_dataset.polygons.model_dump_json(),
+            dataset_type=ds_type,
+        )
     return db_id
 
 
 def _get_n_periods(health_dataset):
-    frequency = "M" if isinstance(health_dataset.period_range[0], Month) else "W"
-    n_periods = 3 if frequency == "M" else 12
+    frequency = 'M' if isinstance(health_dataset.period_range[0], Month) else 'W'
+    n_periods = 3 if frequency == 'M' else 12
     return n_periods
 
 
