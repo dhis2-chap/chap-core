33d78b5d94ed72e897d1d84f85ab36a681d69357
knutdrand <knutdrand@gmail.com>
2025-09-11 14:04:06 +0200

    feat: endpoint for metrics + backtestid in visualization

--- chap_core/rest_api/v1/routers/visualization.py
+++ chap_core/rest_api/v1/routers/visualization.py
@@ -21,16 +21,38 @@ plot_registry = {"metric_by_horizon": MetricByHorizon, "metric_map": MetricMap}
 
 
 # List visualizations
-@router.get("/", response_model=list[str])
-def list_visualizations():
+@router.get("/{backtest_id}", response_model=list[str])
+def list_visualizations(backtest_id: int):
     """
     List available visualizations
     """
     return list(plot_registry.keys())
 
 class VisualizationParams(pydantic.BaseModel):
-
     metric_id: int
+
+class Metric(pydantic.BaseModel):
+    metric_id: str
+    display_name: str
+    description: str = ''
+
+metrics = [Metric(metric_id= 'crps',
+                  display_name='CRPS',
+                  description='Checking if the sampled distribuion matches the truth'),
+           Metric(metric_id= 'crps_norm',
+                  display_name='CRPS norm',
+                  description='Checking'),
+           Metric(metric_id= 'is_within_10th_90th',
+                  display_name='Within 10th 90th percentile'),
+           Metric(metric_id= 'is_within_25th_75th',
+                  display_name='Within 25th 75th percentile'),
+           Metric(metric_id= 'is_within_25th_75th',
+                  display_name='Within 25th 75th percentile')]
+
+@router.get("/metrics/{backtest_id}", response_model=list[Metric])
+def get_available_metrics(backtest_id: int):
+    return metrics
+
 
 @router.get("/{visualization_name}/{backtest_id}/{metric_id}")
 def generate_visualization(
@@ -48,7 +70,6 @@ def generate_visualization(
     print(metric_id)
     metrics = [metric for metric in all_metrics
                if metric.metric_id == metric_id]
-    print(metrics)
     plt_class = plot_registry[visualization_name]
     plot_spec = plt_class(metrics, geojson).plot_spec()
     return JSONResponse(plot_spec)
