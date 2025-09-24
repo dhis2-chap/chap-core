2a57922908752ace2c5a4d8a85c187fb7b94029e
Ivar Grytten <ivar.grytten@gmail.com>
2025-09-24 11:27:42 +0200

    fix: removed unused test file

--- tests/evaluation/test_evaluation_plots.py
+++
@@ -1,7 +0,0 @@
-from chap_core.assessment.metrics import DetailedRMSE
-from chap_core.plotting.evaluation_plot import MetricByHorizonV2, make_plot_from_backtest_object
-
-
-def test_evaluation_plot_from_backtest_object(backtest):
-    plot = make_plot_from_backtest_object(backtest, MetricByHorizonV2, DetailedRMSE())
-    # plot.show()
